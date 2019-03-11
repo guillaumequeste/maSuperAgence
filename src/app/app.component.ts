432344c5e1cbe7643384c5e919649bb44e91a1e1
Guillaume Queste <guillaume.queste@laposte.net>
2019-03-11 09:49:00 +0100

    configuration firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maSuperAgence';
+
+  constructor() {
+    // Initialize Firebase
+    const config = {
+      apiKey: 'AIzaSyDe-S3VlBrKHeW5shuucfCKth4C74V6_Bc',
+      authDomain: 'masuperagence-dac83.firebaseapp.com',
+      databaseURL: 'https://masuperagence-dac83.firebaseio.com',
+      projectId: 'masuperagence-dac83',
+      storageBucket: 'masuperagence-dac83.appspot.com',
+      messagingSenderId: '589760904942'
+    };
+    firebase.initializeApp(config);
+  }
+
 }
