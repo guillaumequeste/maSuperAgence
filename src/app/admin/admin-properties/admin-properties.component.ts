982eabc3ce71d8bf163fa671294b17d3acb13a8d
Guillaume Queste <guillaume.queste@laposte.net>
2019-03-11 18:25:48 +0100

    ajout de plusieurs images pour chaque bien

--- src/app/admin/admin-properties/admin-properties.component.ts
+++ src/app/admin/admin-properties/admin-properties.component.ts
@@ -17,8 +17,8 @@ export class AdminPropertiesComponent implements OnInit, OnDestroy {
   propertiesSubscription: Subscription;
   editProperty = false;
   photoUploading = false;
-  photoUrl: string;
   photoUploaded = false;
+  photosAdded: any[] = [];
 
   constructor(private formBuilder: FormBuilder, private propertiesService: PropertiesService) { }
 
@@ -48,6 +48,9 @@ export class AdminPropertiesComponent implements OnInit, OnDestroy {
   resetPropertyForm() {
     this.editProperty = false;
     this.propertyForm.reset();
+    this.photosAdded = [];
+    this.photoUploaded = false;
+    this.photoUploading = false;
   }
 
   onSaveProperty() {
@@ -57,10 +60,8 @@ export class AdminPropertiesComponent implements OnInit, OnDestroy {
     const surface = this.propertyForm.get('surface').value;
     const rooms = this.propertyForm.get('rooms').value;
     const description = this.propertyForm.get('description').value;
-    const newProperty = new Property(title, category, surface, rooms, description);
-    if (this.photoUrl && this.photoUrl !== '') {
-      newProperty.photo = this.photoUrl;
-    }
+    const photos = this.photosAdded ? this.photosAdded : [];
+    const newProperty = new Property(title, category, surface, rooms, description, photos);
 
     if (this.editProperty === true) {
       this.propertiesService.updateProperty(newProperty, id);
@@ -69,8 +70,6 @@ export class AdminPropertiesComponent implements OnInit, OnDestroy {
     }
     $('#propertiesFormModal').modal('hide');
     this.resetPropertyForm();
-    this.photoUploaded = false;
-    this.photoUrl = '';
   }
 
   ngOnDestroy() {
@@ -78,10 +77,12 @@ export class AdminPropertiesComponent implements OnInit, OnDestroy {
   }
 
   onDeleteProperty(property: Property) {
-    this.propertiesService.removeProperty(property);
-    if (property.photo) {
-      this.propertiesService.removePropertyPhoto(property.photo);
+    if (property.photos) {
+      property.photos.forEach(photo => {
+        this.propertiesService.removePropertyPhoto(photo);
+      });
     }
+    this.propertiesService.removeProperty(property);
   }
 
   onEditProperty(property: Property, id: number) {
@@ -92,18 +93,29 @@ export class AdminPropertiesComponent implements OnInit, OnDestroy {
     this.propertyForm.get('surface').setValue(property.surface);
     this.propertyForm.get('rooms').setValue(property.rooms);
     this.propertyForm.get('description').setValue(property.description);
+    this.photosAdded = property.photos;
     this.editProperty = true;
   }
 
   detectFile(event) {
+    this.photoUploaded = false;
     this.photoUploading = true;
     this.propertiesService.uploadFile(event.target.files[0]).then(
       (url: string) => {
-        this.photoUrl = url;
+        this.onAddPhoto(url);
         this.photoUploading = false;
         this.photoUploaded = true;
       }
     );
+  }
+
+  onAddPhoto(url: string) {
+    this.photosAdded.push(url);
+  }
+
+  onRemoveAddedPhoto(id: number) {
+    this.propertiesService.removePropertyPhoto(this.photosAdded[id]);
+    this.photosAdded.splice(id, 1);
   }
 
 }
--- src/app/models/Property.model.ts
+++ src/app/models/Property.model.ts
@@ -1,8 +1,8 @@
 export class Property {
-    photo: string;
     constructor(public title: string,
                 public category: string,
                 public surface: string,
                 public rooms: string,
-                public description: string) {}
+                public description: string,
+                public photos: any[]) {}
 }
