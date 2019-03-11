aab81043f5cefbc31f6f849354727dbad3e875aa
Guillaume Queste <guillaume.queste@laposte.net>
2019-03-11 15:24:13 +0100

    créer un bien immobilier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AdminSigninComponent } from './admin/admin-signin/admin-signin.component';
 import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { AdminPropertiesComponent } from './admin/admin-properties/admin-properties.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AdminSigninComponent,
-    AdminDashboardComponent
+    AdminDashboardComponent,
+    AdminPropertiesComponent
   ],
   imports: [
     BrowserModule,
